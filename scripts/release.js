b5451bc9ad733c14f6d293b399895fcd46434467
toyobayashi <lifenglin314@outlook.com>
2022-05-15 00:47:11 +0800

    try

--- scripts/release.js
+++ scripts/release.js
@@ -4,10 +4,14 @@ import path from 'path'
 const __dirname = path.dirname(import.meta.url.substring(8))
 
 async function build (p) {
-  await execa('npm', ['publish'], {
-    stdio: 'inherit',
-    cwd: path.join(__dirname, '../packages', p)
-  })
+  try {
+    await execa('npm', ['publish'], {
+      stdio: 'inherit',
+      cwd: path.join(__dirname, '../packages', p)
+    })
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 await build('tsapi')
